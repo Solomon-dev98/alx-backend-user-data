b52c3f7e383254f7559d6709dc905ff9044328c5
Solomon-dev98 <asapnino01@gmail.com>
2024-11-12 14:16:10 +0100

    feat: update the method authorization header

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -11,7 +11,21 @@ import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+# CORS setup
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+# Initialize auth variable
+auth = None
+
+# check if AUTH_TYPE is set in environment variables
+# and load the appropriate authentication class
+if os.getenv("AUTH_TYPE"):
+    auth_type = os.getenv("AUTH_TYPE")
+
+    if auth_type == "auth":
+        from api.v1.auth.auth import Auth
+        auth = Auth()
 
 
 @app.errorhandler(404)
@@ -33,6 +47,31 @@ def forbidden(error) -> str:
     """Forbidden error handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """Filter requests before they are processed"""
+    if auth is None:
+        return
+    # list of exempt paths
+    exempt_paths = ['/api/v1/status/',
+                     '/api/v1/unauthorized/',
+                     '/api/v1/forbidden/']
+
+    # if the request path is not in the exempt list, check authentication
+    if request.path not in exempt_paths:
+
+        # check if authentication is required for the current path
+        if auth.require_auth(request.path, exempt_paths):
+
+            # if the authorization is required for the current path
+            if auth.authorization_header(request) is None:
+                abort(401)  # Unauthorized
+
+            # if no current user is found, abort with 403
+            if auth.current_user(request) is None:
+                abort(403)  # Forbidden
 
 
 if __name__ == "__main__":
