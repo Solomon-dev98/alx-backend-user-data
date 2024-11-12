7d9a12237cfabc14849c9e1fdb37bfcf3c1f3fe9
Solomon-dev98 <asapnino01@gmail.com>
2024-11-12 14:52:58 +0100

    feat: Add a class BasicAuth that inherits from Auth, an empty class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# api/v1/auth/basic_auth.py
+"""Implementation for a class for BasicAuth"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class that inherits from Auth"""
+    pass
