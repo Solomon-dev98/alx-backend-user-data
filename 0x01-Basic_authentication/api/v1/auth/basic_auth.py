dcb6f5910ea067a3b0fcdaee5bed28d02c812069
Solomon-dev98 <asapnino01@gmail.com>
2024-11-13 13:19:32 +0100

    Add method that returns the decoded value of a Base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 # api/v1/auth/basic_auth.py
 """Implementation for a class for BasicAuth"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -34,3 +35,34 @@ class BasicAuth(Auth):
 
         # Return the part after "Basic "
         return authorization_header[6:]  # skip "Basic " (6 chars)
+
+    def decode_base64_authorization_header(
+     self, base64_authorization_header: str) -> str:
+        """A method that returns the decoded value of a Base64 string.
+
+        Args:
+            - base64_authorization_header (str): the string to be decoded.
+
+        Returns:
+            str: returns the decoded base64 string.
+        """
+
+        # check for if base64_authorization_header is None
+        if base64_authorization_header is None:
+            return None
+
+        # check if base64_authorization_header is not a string.
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        # check for if base64_authorization_header is not a valid Base64
+        try:
+            # Validate the base64 part by decoding
+            decoded = base64.b64decode(base64_authorization_header).decode('utf-8')
+
+            # Try to decode the byte string to UTF-8 and return it.
+        except (base64.binascii.Error, UnicodeDecodeError):
+            # if decoding fails, return None
+            return None
+
+        return decoded
